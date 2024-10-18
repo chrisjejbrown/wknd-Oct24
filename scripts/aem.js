eaa50c14150581a908e1bfe4966f96ec2e7cd280
Chris Brown <chrbrown@adobe.com>
2024-10-18 15:41:23 -0400

    "Added new function updateSectionsStatus to update section status in a container element and called it from loadBlocks function."

--- scripts/aem.js
+++ scripts/aem.js
@@ -716,6 +716,29 @@ async function loadBlocks(main) {
     updateSectionsStatus(main);
   }
 }
+/**
+ * Updates all section status in a container element.
+ * @param {Element} main The container element
+ */
+function updateSectionsStatus(main) {
+  const sections = [...main.querySelectorAll(':scope > div.section')];
+  for (let i = 0; i < sections.length; i += 1) {
+    const section = sections[i];
+    const status = section.dataset.sectionStatus;
+    if (status !== 'loaded') {
+      const loadingBlock = section.querySelector(
+        '.block[data-block-status="initialized"], .block[data-block-status="loading"]',
+      );
+      if (loadingBlock) {
+        section.dataset.sectionStatus = 'loading';
+        break;
+      } else {
+        section.dataset.sectionStatus = 'loaded';
+        section.style.display = null;
+      }
+    }
+  }
+}
 init();
 
 export {
